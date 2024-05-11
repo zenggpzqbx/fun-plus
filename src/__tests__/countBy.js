2d2635fbe2e9b8b80c712a0f229d5122113289fb
zenggpzqbx <zenggpzqbx@163.com>
2024-05-12 00:38:15 +0800

    chore: 测试文件修改

--- src/__tests__/countBy.js
+++ src/__tests__/countBy.js
@@ -1,7 +1,10 @@
 import {countBy} from "../source";
-test('第一个用例', () => {
-    expect(countBy([{zmb:'123'}, {zmb:'123'}, {zmb:'12344'}],  item => item.zmb)).toEqual({ '123': 2, '12344': 1 });
+
+describe('count', () => {
+    test('第一个用例', () => {
+        expect(countBy([{zmb:'123'}, {zmb:'123'}, {zmb:'12344'}],  item => item.zmb)).toEqual({ '123': 2, '12344': 1 });
+    });
+    test('第二个用例', () => {
+        expect(countBy([6.1, 4.2, 6.3],  Math.floor)).toEqual({'6': 2,'4': 1});
+    })
 });
-test('第二个用例', () => {
-    expect(countBy([6.1, 4.2, 6.3],  Math.floor)).toEqual({'6': 2,'4': 1});
-})
--- src/__tests__/pick.js
+++ src/__tests__/pick.js
@@ -1,8 +1,11 @@
 import {pick} from "../source";
-test('第一个用例', () => {
-    expect(pick({name:'abc', age:28, sex:'男'}, 'name')).toEqual({ 'name': 'abc'});
-});
 
-test('第二个用例', () => {
-    expect(pick({name:'abc', age:28, sex:'男'}, ['name', 'age'])).toEqual({ 'name': 'abc', 'age': 28 });
+describe('pick', () => {
+    test('第一个用例', () => {
+        expect(pick({name:'abc', age:28, sex:'男'}, 'name')).toEqual({ 'name': 'abc'});
+    });
+
+    test('第二个用例', () => {
+        expect(pick({name:'abc', age:28, sex:'男'}, ['name', 'age'])).toEqual({ 'name': 'abc', 'age': 28 });
+    });
 });
